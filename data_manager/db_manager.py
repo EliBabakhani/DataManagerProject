b07373455cb36fe2d0fba32019bf9aa8828851c2
EliBabakhani <elnaz.babakhani71@gmail.com>
2023-07-05 10:00:46 -0700

    database

--- data_manager/db_manager.py
+++ data_manager/db_manager.py
@@ -1,10 +1,12 @@
 from .base import BaseManager, BaseModel
 import psycopg2 as pg
 
+
 class DBManager(BaseManager):
-    
+
     def __init__(self, config: dict) -> None:
-        super().__init__(config)  # {'db_config':{'dbname':'', 'host':'', 'password':'', 'user':'', ...}}
+        # {'db_config':{'dbname':'', 'host':'', 'password':'', 'user':'', ...}}
+        super().__init__(config)
         self._db_config = config['db_config']
         self.__conn = pg.connect(**self._db_config)
 
@@ -17,68 +19,75 @@ class DBManager(BaseManager):
         else:
             return str(value)
 
-    
     def create_table(self, model_cls: type):
         assert issubclass(model_cls, BaseModel)
 
         with self.__conn.cursor() as curs:
             cols_dict = model_cls._get_columns()
             sql_cols = ','.join([" ".join(v) for v in cols_dict.values()])
-            curs.execute(f"CREATE TABLE {model_cls.TABLE_NAME} ({sql_cols});", )
-        
+            curs.execute(
+                f"CREATE TABLE {model_cls.TABLE_NAME} ({sql_cols});", )
+
         self.__conn.commit()
-    
+
     def _check_table_exists(self,  model_cls: type):
         with self.__conn.cursor() as curs:
             curs.execute("SELECT * FROM information_schema.tables WHERE table_name=%s",
-                        (model_cls.TABLE_NAME,))
+                         (model_cls.TABLE_NAME,))
             return bool(curs.fetchone())
-
 
     def create(self, m: BaseModel):
         if not self._check_table_exists(m.__class__):
             self.create_table(m.__class__)
-        
+
         model_data = m.to_dict()  # {'_id':1, 'username':'akbar', ...}
         converter = self.converter_model_to_query
 
         with self.__conn.cursor() as curs:
             keys = ','.join(model_data.keys())
-            values = ','.join(map(converter, model_data.values())) # 1, 'akbar', 'akbar1',... -> 1, 'akbar', 'akbar1' -> "1, 'akbar', 'akbar1'"
-            curs.execute(f"INSERT INTO {m.TABLE_NAME} ({keys}) VALUES ({values}) RETURNING _id")
+            # 1, 'akbar', 'akbar1',... -> 1, 'akbar', 'akbar1' -> "1, 'akbar', 'akbar1'"
+            values = ','.join(map(converter, model_data.values()))
+            curs.execute(
+                f"INSERT INTO {m.TABLE_NAME} ({keys}) VALUES ({values}) RETURNING _id")
             new_model_id = curs.fetchone()
             m._id = new_model_id
-        
+
         self.__conn.commit()
         return new_model_id
 
-       
-        
-
     def read(self, id: int, model_cls: type) -> BaseModel:
-        # TODO: Complete
-        pass
-
+        with self.__conn.cursor() as curs:
+            curs.execute(
+                f"SELECT * FROM {model_cls.TABLE_NAME} WHERE '_id'={id};")
+            read_row = curs.fetchall()
+        self.__conn.commit()
 
     def update(self, m: BaseModel) -> None:
-        # TODO: Complete
-        pass
-
+        with self.__conn.cursor() as curs:
+            model_data = m.to_dict()
+            converter = self.converter_model_to_query
+            keys = ','.join(model_data.keys())
+            values = ','.join(map(converter, model_data.values()))
+            # curs.execute(f"INSERT INTO {m.TABLE_NAME} ({keys}) VALUES ({values}) RETURNING _id")
+            curs.execute(
+                f" UPDATE {m.TABLE_NAME} SET {keys} WHERE '_id'= {m._id}", ({values}))
+        self.__conn.commit()
 
     def delete(self, id: int, model_cls: type) -> None:
-        # TODO: Complete
-        pass
-
-
+        with self.__conn.cursor() as curs:
+            curs.execute(
+                f"DELETE FROM {model_cls.TABLE_NAME} WHERE '_id'={id};")
+        self.__conn.commit()
 
     def read_all(self, model_cls: type):
-        # TODO: Complete
-        pass
-
+        with self.__conn.cursor() as curs:
+            curs.execute(
+                f"SELECT * FROM {model_cls.TABLE_NAME} WHERE '{model_cls}'={model_cls.__name__};")
+            read_row = curs.fetchall()
+        self.__conn.commit()
 
     def truncate(self, model_cls: type) -> None:
-        #TODO: Complete
-        pass
-
-    
-    
+        with self.__conn.cursor() as curs:
+            curs.execute(
+                f"DELETE FROM {model_cls.TABLE_NAME} WHERE '{model_cls}'={model_cls.__name__};")
+        self.__conn.commit()
